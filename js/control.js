4084f16bdf3affa9774083f96fb52ff64f447261
Ulyana <buskoulya@gmail.com>
2018-03-08 12:31:12 +0300

    open modal to call

--- js/control.js
+++ js/control.js
@@ -1,5 +1,7 @@
 $(function(){
     'use strict';
+    
+    //scroll to nav
     $('a[href^="#"]').click(function(){
         let target = $(this).attr('href');
         $('html, body').animate({
@@ -9,6 +11,7 @@ $(function(){
     
     var offset = $('nav').offset();
     
+    //fixed nav
     $(window).scroll(function(){
         if($(window).scrollTop() > offset.top) {
             $('#bottom_header').addClass('fixedNav');
@@ -17,6 +20,7 @@ $(function(){
         }
     });
     
+    // slide testimonials
     if( window.innerWidth > 1024) {
         $('#slide').slick({
             slidesToShow: 3,
@@ -33,11 +37,22 @@ $(function(){
         });
     }
     
+    // show all img box
     $('#view_all_gallery').click(function(){
         let content = '<div class="gallery_line main_flex__nowrap flex__jcontent_between"><div class="box_img_gallery"><img src="img/gallery/1.jpg" alt="1"></div><div class="box_img_gallery"><img src="img/gallery/2.jpg" alt="2"></div></div><div class="name_gallery">Ulyana Busko / <span class="blue">North Miami Beach, FL 33162</span></div>';
         $('#gallery_box').append(content);
         $(this).remove();
     });
     
+    
+    // open modal to call
+    $('#modal_box, #black_fill').hide();
+    $('.call').click(function(){
+        $('#modal_box, #black_fill').show();
+    });
+    
+    $('.close_modal, #black_fill').click(function(){
+        $('#modal_box, #black_fill').hide();
+    });
 
 });
